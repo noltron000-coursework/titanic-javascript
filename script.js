2017e192f775d7d61e882323c370b1a40cd493ec
Nolan Kovacik <noltron000@gmail.com>
2020-01-27 14:20:32 -0800

    [doc] passenger info

--- script.js
+++ script.js
@@ -3,39 +3,47 @@ const body = document.querySelector('body')
 const html = document.querySelector('html')
 const container = document.querySelector('#container')
 
+// Define new class for passengers.
+// The inputs are pretty ugly so this will fix it up.
 class Passenger {
+	// A row of data in JSON represents a passenger.
+	// A passanger is just a person with several qualities,
+	// including their ticket purchase and personal info.
 	constructor(entry) {
-		// Cleaned metadata
+		// Cleaned metadata goes here.
 		this.dataID = entry['datasetid']
 		this.ticketID = entry['fields']['ticket']
 		this.passengerID = entry['fields']['passengerid']
 		this.recordID = entry['recordid']
 		this.recordTime = entry['record_timestamp']
 
-		// Passenger information
+		// Passenger - personal information.
 		this.name = entry['fields']['name']
 		this.age = entry['fields']['age']
 		this.sex = entry['fields']['sex']
-		this.fare = entry['fields']['fare']
-		this.class = entry['fields']['pclass']
-		this.cabin = entry['fields']['cabin']
-		this.embarked = entry['fields']['embarked']
 		// This field includes both siblings & spouses.
 		this.numSiblings = entry['fields']['sibsp']
 		// This field includes both children & parents.
 		this.numChildren = entry['fields']['parch']
 		// This field is a string where it should be boolean.
 		this.survived = this._cleanSurvived(entry)
+		
+		// Passenger - ticket information.
+		this.fare = entry['fields']['fare']
+		this.class = entry['fields']['pclass']
+		this.cabin = entry['fields']['cabin']
+		this.embarked = entry['fields']['embarked']
 	}
 
 	_cleanSurvived = (entry) => {
+		// This internal function cleans the "survived" field.
 		switch (entry['fields']['survived']) {
 			case 'Yes':
 				return true
 			case 'No':
 				return false
 			default:
-				return undefined
+				return null
 		}
 	}
 }
