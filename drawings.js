89bfc87c992998789032c7b814fee22f438e4294
Nolan Kovacik <noltron000@gmail.com>
2020-02-05 11:35:02 -0800

    [add] new drawings class

--- drawings.js
+++ drawings.js
@@ -1,27 +1,38 @@
 const drawData = (T) => {
-	console.log('hello')
-	noFilter(T)
+	Dataset = new DataDrawing(T)
 }
 
-const renderNode = (passenger) => {
-	const parent = document.getElementById('container')
-	const div = document.createElement('div')
-	div.classList.add('passenger')
-	parent.appendChild(div)
-	
-}
+class DataDrawing {
+	constructor(T) {
+		this.data = T.data
+		this.createNodes()
+		this.renderNodes()
+	}
 
-const noFilter = (T) => {
-	T.data.forEach((passenger) => {
-		renderNode(passenger)
-	})
-}
+	createNodes = () => {
+		// elements array will contain all the passenger nodes.
+		const elements = this.data.map((passenger, index) => {
+			const div = document.createElement('div')
+			div.classList.add('passenger')
+			div.setAttribute('data-index', index)
+			return div
+		})
+		this.nodes = elements
+	}
 
-const filterByGender = (T) => {
-	const men = T.data.filter((passenger) => {
-		return passenger.sex == 'male'
-	})
-	const women = T.data.filter((passenger) => {
-		return passenger.sex == 'female'
-	})	
+	renderNodes = () => {
+		const parent = document.getElementById('container')
+		this.nodes.forEach((div) => {
+			parent.appendChild(div)
+		})
+	}
+
+	filterByGender = (T) => {
+		const men = T.data.filter((passenger) => {
+			return passenger.sex == 'male'
+		})
+		const women = T.data.filter((passenger) => {
+			return passenger.sex == 'female'
+		})
+	}
 }
